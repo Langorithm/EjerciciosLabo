6a9e14e6be64d61c169278555697874a86a3c3e2
Langorithm <sebastian.lang.maso@gmail.com>
2019-07-03 20:55:41 -0300

    DiccString Inicial

---
+++ taller_diccionario_sobre_trie/src/string_map.hpp
@@ -0,0 +1,77 @@
+//FUNCIONES AUXILIARES
+vector<int> _str2Vec(const string& s) {
+    vector<int> v(s.size());
+
+    for (int i = 0; i < s.size(); ++i) {
+        v[i] = int(s[i]);
+    }
+    return v;
+}
+
+
+template <typename T>
+string_map<T>::string_map(){
+    _raiz = new Nodo;
+    _size = 0;
+}
+
+template <typename T>
+string_map<T>::string_map(const string_map<T>& aCopiar) : string_map() { *this = aCopiar; } // Provisto por la catedra: utiliza el operador asignacion para realizar la copia.
+
+template <typename T>
+string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
+    // COMPLETAR
+}
+
+template <typename T>
+string_map<T>::~string_map(){
+    // COMPLETAR
+}
+
+template <typename T>
+T& string_map<T>::operator[](const string& clave){
+    // COMPLETAR
+}
+
+
+template <typename T>
+int string_map<T>::count(const string& clave) const{
+    bool esta = true;
+    vector<int> vClave = _str2Vec(clave);
+    Nodo* p = _raiz;
+
+
+    for (int i = 0; esta && i < vClave.size(); ++i) {
+        if (p->siguientes[i])
+            p = p->siguientes[i];
+        else
+            esta = false;
+    }
+
+    return int(esta);
+}
+
+template <typename T>
+const T& string_map<T>::at(const string& clave) const {
+    // COMPLETAR
+}
+
+template <typename T>
+T& string_map<T>::at(const string& clave) {
+    // COMPLETAR
+}
+
+template <typename T>
+void string_map<T>::erase(const string& clave) {
+    // COMPLETAR
+}
+
+template <typename T>
+int string_map<T>::size() const{
+    // COMPLETAR
+}
+
+template <typename T>
+bool string_map<T>::empty() const{
+    // COMPLETAR
+}
